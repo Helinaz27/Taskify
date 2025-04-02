410d3d6c9e067dcbe3572e9f56df1bb9ddfa9c54
unknown <helinazeleke670@gmail.com>
2025-04-02 22:27:05 +0300

    added Taskify

---
+++ script.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById("mobilemenu");
+const mobilenav = document.getElementById("mobilenav");
+
+mobilenav.style.left = "-100%";
+
+btn.onclick = function () {
+  if (mobilenav.style.left === "-100%") {
+    mobilenav.style.left = "50%";
+    btn.src = "images/icon-close.svg";
+  } else {
+    mobilenav.style.left = "-100%";
+    btn.src = "images/icon-hamburger.svg";
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const connectItems = document.querySelectorAll(".connect-text");
+
+  connectItems.forEach(item => {
+    item.addEventListener("click", function () {
+      const dropdown = this.nextElementSibling;
+      dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+    });
+  });
+});
